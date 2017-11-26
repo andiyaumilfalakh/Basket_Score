b8614bf24f70f386a56f6b77fd37a507db9e2e52
aif-PC\aif <andiyaumil69@gmail.com>
2017-11-26 11:15:14 +0800

    First Commit

---
+++ app/src/main/java/com/example/aif/basketscore/BasketScore.java
@@ -0,0 +1,13 @@
+package com.example.aif.basketscore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BasketScore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basket_score);
+    }
+}
